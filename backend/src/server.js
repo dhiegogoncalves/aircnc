641d6021e5eb84808114c908ea71fd1276fea030
Dhiego Gonçalves da Silva <dhhiego@gmail.com>
2019-10-05 15:52:04 -0400

    Configuração Env, Cors e Mongoose

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,17 @@
+require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const routes = require('./routes');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-
 app.listen(3333);
